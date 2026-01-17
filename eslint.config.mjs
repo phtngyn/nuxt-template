6b8318d1aa70225866aec7cd0a13cc767564e83d
phatnguyen <phatnguyen@gmx.de>
2026-01-17 12:51:46 +0100

    chore: bumps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,10 @@ import antfu from '@antfu/eslint-config'
 import nuxt from './.nuxt/eslint.config.mjs'
 
 export default nuxt(
-  antfu(
-    {
-      formatters: true,
-      rules: {
-        'no-console': 'off',
-      },
+  antfu({
+    formatters: true,
+    rules: {
+      'no-console': 'off',
     },
-  ),
+  }),
 )
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,3 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
     '@nuxt/eslint',
@@ -21,15 +20,6 @@ export default defineNuxtConfig({
   },
 
   compatibilityDate: '2025-05-01',
-
-  typescript: {
-    tsConfig: {
-      compilerOptions: {
-        noUncheckedIndexedAccess: true,
-        exactOptionalPropertyTypes: true,
-      },
-    },
-  },
 
   eslint: {
     config: {
